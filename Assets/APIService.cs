4c633b826cc239c6b13ebf5ce1094ddcea3ba26e
Pedro Calero <pedrocaleromontalban@gmail.com>
2020-07-12 13:33:03 +0200

    Api service created.

---
+++ Assets/APIService.cs
@@ -0,0 +1,202 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class APIService : MonoBehaviour
+{
+    private static APIService instance;
+
+    MongoClient client;
+    IMongoDatabase database;
+
+    IMongoCollection<BsonDocument> sudokuCollection;
+    IMongoCollection<BsonDocument> squareCollection;
+    IMongoCollection<BsonDocument> timerCollection;
+    IMongoCollection<BsonDocument> responsesCollection;
+
+    public static APIService GetInstance()
+    {
+        return instance;
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        client = new MongoClient("mongodb+srv://test:tfg@cluster0-ayt30.mongodb.net/test?retryWrites=true&w=majority");
+        database = client.GetDatabase("test");
+
+        sudokuCollection = database.GetCollection<BsonDocument>("testHost");
+        responsesCollection = database.GetCollection<BsonDocument>("responses");
+        squareCollection = database.GetCollection<BsonDocument>("square");
+        timerCollection = database.GetCollection<BsonDocument>("testHostTimer");
+    }
+
+    public List<BsonDocument> FindAllDocuments(Collections collection)
+    {
+        return FindAllDocuments(GetCollectionFromEnum(collection));
+    }
+
+    private List<BsonDocument> FindAllDocuments(IMongoCollection<BsonDocument> collection)
+    {
+        return collection.Find(new BsonDocument()).ToList();
+    }
+
+    public void UploadDocument(Collections collection, BsonDocument document)
+    {
+        UploadDocument(GetCollectionFromEnum(collection), document);
+    }
+
+    private void UploadDocument(IMongoCollection<BsonDocument> collection, BsonDocument document)
+    {
+        collection.InsertOne(document);
+    }
+
+    public void ClearCollection(Collections collection)
+    {
+        ClearCollection(GetCollectionFromEnum(collection));
+    }
+
+    private void ClearCollection(IMongoCollection<BsonDocument> collection)
+    {
+        var filter = Builders<BsonDocument>.Filter.Empty;
+
+        collection.DeleteMany(filter);
+    }
+
+    private IMongoCollection<BsonDocument> GetCollectionFromEnum(Collections collections)
+    {
+        switch (collections)
+        {
+            case Collections.Sudoku:
+                return sudokuCollection;
+            case Collections.Square:
+                return squareCollection;
+            case Collections.Timer:
+                return timerCollection;
+            case Collections.Response:
+                return responsesCollection;
+        }
+
+        return null;
+    }
+}
+
+public enum Collections
+{
+    Sudoku,
+    Response,
+    Square,
+    Timer
+}
+
+
+
+[BsonDiscriminator("square")]
+public class SquareDocument
+{
+    [BsonElement]
+    public Int32 x;
+    [BsonElement]
+    public Int32 y;
+
+    public SquareDocument(Int32 x, Int32 y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+[BsonDiscriminator("response")]
+public class ResponseDocument
+{
+    [BsonElement]
+    public Int32 number;
+
+    public ResponseDocument(Int32 number)
+    {
+        this.number = number;
+    }
+}
+
+[BsonDiscriminator("sudoku")]
+public class SudokuDocument
+{
+    [BsonElement("sudoku")]
+    public string[] sudoku;
+
+    public SudokuDocument(string[] sudoku)
+    {
+        this.sudoku = sudoku;
+    }
+}
+
+[BsonDiscriminator("timer")]
+public class TimerDocument
+{
+    [BsonElement("timer")]
+    public float timer;
+
+    public TimerDocument(float timer)
+    {
+        this.timer = timer;
+    }
+}
+
+[BsonDiscriminator("coordinates")]
+public class CoordinatesDocument
+{
+    [BsonElement("x")]
+    public int x;
+    [BsonElement("y")]
+    public int y;
+
+    public CoordinatesDocument(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+[Serializable]
+public class SudokuJson
+{
+    public float timer;
+    public string[] sudoku;
+
+    public SudokuJson(string[] sudoku, float timer)
+    {
+        this.sudoku = sudoku;
+    }
+}
+
+[Serializable]
+public class TimerJson
+{
+    public float timer;
+
+    public TimerJson(float timer)
+    {
+        this.timer = timer;
+    }
+}
+
+[Serializable]
+public class CoordinatesJson
+{
+    public int x;
+    public int y;
+
+    public CoordinatesJson(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
--- Assets/HostManager.cs
+++ Assets/HostManager.cs
@@ -1,10 +1,7 @@
 ﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-using MongoDB.Driver;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using UnityEngine;
@@ -21,48 +18,36 @@ public class HostManager : MonoBehaviour
     [SerializeField]
     Text serverTimer;
 
-    MongoClient client;
-    IMongoDatabase database;
-
-    IMongoCollection<BsonDocument> sudokuCollection;
-    IMongoCollection<BsonDocument> squareCollection;
-    IMongoCollection<BsonDocument> timerCollection;
-    IMongoCollection<BsonDocument> responsesCollection;
+    APIService apiService;
 
     private float timer = 40;
 
     public void Start()
     {
-        client = new MongoClient("mongodb+srv://test:tfg@cluster0-ayt30.mongodb.net/test?retryWrites=true&w=majority");
-        database = client.GetDatabase("test");
-        sudokuCollection = database.GetCollection<BsonDocument>("testHost");
+        apiService = APIService.GetInstance();
 
-        //Create new sudoku
-        CreateNewSudoku(sudokuCollection);
+        CreateNewSudoku();
 
         //Clean response collection
-        responsesCollection = database.GetCollection<BsonDocument>("responses");
-        ClearCollection(responsesCollection);
+        apiService.ClearCollection(Collections.Response);
 
         //CreateNewSquare
-        squareCollection = database.GetCollection<BsonDocument>("square");
-        UploadNewSquare(squareCollection);
+        UploadNewSquare();
 
         //Start timer
-        timerCollection = database.GetCollection<BsonDocument>("testHostTimer");
-        ClearCollection(timerCollection);
+        apiService.ClearCollection(Collections.Timer);
 
         var timerDocument = new TimerDocument(timer);
-        UploadTimer(timerCollection, timerDocument);
+        UploadTimer( timerDocument);
     }
 
-    private void CreateNewSudoku(IMongoCollection<BsonDocument> sudokuCollection)
+    private void CreateNewSudoku()
     {
         var sudokuGenerator = new SudokuGenerator();
         var newSudoku = sudokuGenerator.GenerateSudoku(5, 9);
         var sudokuDocument = new SudokuDocument(newSudoku);
 
-        UploadSudoku(sudokuCollection, sudokuDocument);
+        ClearAndUploadSudoku(sudokuDocument);
 
         sudokuManager.SetNewSudoku(newSudoku);
     }
@@ -83,26 +68,31 @@ public class HostManager : MonoBehaviour
             localTimer.text = timer.ToString();
             var timerDocument = new TimerDocument(timer);
 
-            UploadTimer(timerCollection, timerDocument);
+            UploadTimer(timerDocument);
         }
 
-        serverTimer.text = timerCollection.Find(new BsonDocument()).ToList()[0][1].ToString();
+        serverTimer.text = apiService.FindAllDocuments(Collections.Timer)[0][1].ToString();
+    }
+
+    public float GetTimer()
+    {
+        return timer;
     }
 
     public void OnTurnEnded()
     {
-        var responses = responsesCollection.Find(new BsonDocument()).ToList();
+        var responses = apiService.FindAllDocuments(Collections.Response);
 
-        ProcessResponses(responsesCollection, responses, currentSquare);
-        ClearCollection(responsesCollection);
+        ProcessResponses(responses, currentSquare);
+        apiService.ClearCollection(Collections.Response);
 
         var sudokuDocument = new SudokuDocument(SudokuParser.SudokuSquareListToStringArray(sudokuManager.GetSudoku()));
 
-        UploadSudoku(sudokuCollection, sudokuDocument);
-        UploadNewSquare(squareCollection);
+        ClearAndUploadSudoku(sudokuDocument);
+        UploadNewSquare();
     }
 
-    public void ProcessResponses(IMongoCollection<BsonDocument> responsesCollection, List<BsonDocument> responses, Vector2 square)
+    public void ProcessResponses(List<BsonDocument> responses, Vector2 square)
     {
         var array = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
@@ -112,13 +102,6 @@ public class HostManager : MonoBehaviour
         }
 
         sudokuManager.SetNumber((int)square.x, (int)square.y, GetMostVotedResponse(array).ToString());
-    }
-
-    public void UploadTimer(IMongoCollection<BsonDocument> timerCollection, TimerDocument timerDocument)
-    {
-        ClearCollection(timerCollection);
-
-        timerCollection.InsertOne(timerDocument.ToBsonDocument());
     }
 
     public int GetMostVotedResponse(int[] intArray)
@@ -136,193 +119,26 @@ public class HostManager : MonoBehaviour
         return max;
     }
 
-    //TBD: How to parse a sudoku from server: sudokuManager.SetNewSudoku(ParseSudokuString(bsonList[0][1].ToString()));
-
-    public void UploadSudoku(IMongoCollection<BsonDocument> sudokuCollection, SudokuDocument sudokuDocument)
+    public void UploadTimer(TimerDocument timerDocument)
     {
-        ClearCollection(sudokuCollection);
-        sudokuCollection.InsertOne(sudokuDocument.ToBsonDocument());
+        apiService.ClearCollection(Collections.Timer);
+        apiService.UploadDocument(Collections.Timer, timerDocument.ToBsonDocument());
     }
 
-    public void UploadNewSquare(IMongoCollection<BsonDocument> squareCollection)
+    public void ClearAndUploadSudoku(SudokuDocument sudokuDocument)
     {
-        ClearCollection(squareCollection);
+        apiService.ClearCollection(Collections.Sudoku);
+        apiService.UploadDocument(Collections.Sudoku, sudokuDocument.ToBsonDocument());
+    }
+
+    public void UploadNewSquare()
+    {
+        apiService.ClearCollection(Collections.Square);
 
         var square = sudokuManager.GetNextIncompleteSquare();
         var squareDocument = new SquareDocument((int)square.x, (int)square.y);
-        squareCollection.InsertOne(squareDocument.ToBsonDocument());
+        apiService.UploadDocument(Collections.Sudoku, squareDocument.ToBsonDocument());
 
         currentSquare = new Vector2(square.x, square.y);
     }
-
-    public void ClearCollection(IMongoCollection<BsonDocument> collection)
-    {
-        var filter = Builders<BsonDocument>.Filter.Empty;
-
-        collection.DeleteMany(filter);
-    }
-
-    //TODO: Move to Util class
-    static string ConvertStringArrayToString(string[] array)
-    {
-        // Concatenate all the elements into a StringBuilder.
-        StringBuilder builder = new StringBuilder();
-        foreach (string value in array)
-        {
-            builder.Append(value);
-            builder.Append('.');
-        }
-        return builder.ToString();
-    }
-
-    //TODO: Move to Util class
-    public BsonArray ToBsonDocumentArray(IEnumerable list)
-    {
-        var array = new BsonArray();
-        foreach (var item in list)
-        {
-            array.Add(item.ToBson());
-        }
-        return array;
-    }
-
-    public float GetTimer()
-    {
-        return timer;
-    }
-
-    string lastChar = "";
-
-    //TODO: Move to Util class
-    public string[] ParseSudokuString(string sudoku)
-    {
-        List<string> result = new List<string>();
-
-        foreach (char c in sudoku)
-        {
-            string cStr = c.ToString();
-
-            if (isNumeric(c.ToString()))
-            {
-                lastChar = cStr;
-            }
-            else if (cStr == ",")
-            {
-                result.Add(lastChar);
-                lastChar = "";
-            }
-        }
-
-        return result.ToArray();
-    }
-
-    //TODO: Move to Util class
-    public static Boolean isNumeric(string strToCheck)
-    {
-        Regex rg = new Regex(@"^[0-9]*$");
-        return rg.IsMatch(strToCheck);
-    }
 }
-
-[BsonDiscriminator("square")]
-public class SquareDocument
-{
-    [BsonElement]
-    public Int32 x;
-    [BsonElement]
-    public Int32 y;
-
-    public SquareDocument(Int32 x, Int32 y)
-    {
-        this.x = x;
-        this.y = y;
-    }
-}
-
-[BsonDiscriminator("response")]
-public class ResponseDocument
-{
-    [BsonElement]
-    public Int32 number;
-
-    public ResponseDocument(Int32 number)
-    {
-        this.number = number;
-    }
-}
-
-[BsonDiscriminator("sudoku")]
-public class SudokuDocument
-{
-    [BsonElement("sudoku")]
-    public string[] sudoku;
-
-    public SudokuDocument(string[] sudoku)
-    {
-        this.sudoku = sudoku;
-    }
-}
-
-[BsonDiscriminator("timer")]
-public class TimerDocument
-{
-    [BsonElement("timer")]
-    public float timer;
-
-    public TimerDocument(float timer)
-    { 
-        this.timer = timer;
-    }
-}
-
-[BsonDiscriminator("coordinates")]
-public class CoordinatesDocument
-{
-    [BsonElement("x")]
-    public int x;
-    [BsonElement("y")]
-    public int y;
-
-    public CoordinatesDocument(int x, int y)
-    {
-        this.x = x;
-        this.y = y;
-    }
-}
-
-[Serializable]
-public class SudokuJson
-{
-    public float timer;
-    public string[] sudoku;
-
-    public SudokuJson(string[] sudoku, float timer)
-    {
-        this.sudoku = sudoku;
-    }
-}
-
-[Serializable]
-public class TimerJson
-{
-    public float timer;
-
-    public TimerJson(float timer)
-    {
-        this.timer = timer;
-    }
-}
-
-[Serializable]
-public class CoordinatesJson
-{
-    public int x;
-    public int y;
-
-    public CoordinatesJson(int x, int y)
-    {
-        this.x = x;
-        this.y = y;
-    }
-}
-
